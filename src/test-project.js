afd001942031c0cd0b46d7df7af95a601051a5f9
Milan Mladoniczky <6153201+tuplle@users.noreply.github.com>
2025-12-04 23:07:46 +0100

    Add logging functionality and test results management to the test-project script.

--- src/test-project.js
+++ src/test-project.js
@@ -1,15 +1,24 @@
 import * as child_process from 'node:child_process';
 import * as fs from 'node:fs';
 import * as path from 'node:path';
+import * as util from 'node:util';
 
 
 const COMPILATION_OUTPUT = process.env.BIN_OUTPUT || path.join(process.cwd(), "bin", "gamestats");
 const TEST_PROJECT_SOURCE = process.env.TEST_PROJECT_SOURCE || path.join("src", "main.c");
+const LOG_TO_FILE = process.env.LOG_TO_FILE || false;
 const MAX_POINTS = 15;
+var logFile;
+const logStdout = process.stdout;
 
 if (process.argv.length < 3) {
 	console.log("usage: node test-project.js <test project folder>");
 	process.exit(1);
+}
+
+function log(...data) {
+	if (logFile) logFile.write(util.format(...data) + "\n");
+	logStdout.write(util.format(...data) + "\n");
 }
 
 function cleanup() {
@@ -22,18 +31,18 @@ function cleanup() {
 
 function evaluateResults(results) {
 	const pointsPerTest = MAX_POINTS / results.totalScenarios;
-	console.log("points per test: ", pointsPerTest);
+	log("points per test:", pointsPerTest);
 	let totalPoints = 0;
 	results.results.forEach(result => {
 		result.totalTests = Object.keys(result.asserts).length;
 		result.passedPercentage = Object.values(result.asserts).filter(v => v).length / result.totalTests;
 		result.testPoints = pointsPerTest * result.passedPercentage;
-		console.log("test: ", result.scenario, " -> ", result.testPoints.toFixed(2), " points");
+		log("test:", result.scenario, "->", result.testPoints.toFixed(2), "points");
 		totalPoints += result.testPoints;
 	});
 	results.points = Math.ceil(totalPoints);
 	results.percentage = results.points / MAX_POINTS;
-	console.log("project evaluated to", results.points, "/", MAX_POINTS, "points (", (results.percentage * 100).toFixed(2), "%)");
+	log("project evaluated to", results.points, "/", MAX_POINTS, "points (", (results.percentage * 100).toFixed(2), "%)");
 }
 
 const testProjectFolder = process.argv[2];
@@ -44,22 +53,26 @@ if (!fs.existsSync(testProjectSource)) {
 	console.error("test project source not found");
 	process.exit(1);
 }
+const testProjectResultsFolder = path.join(testProjectFolder, "test-results", "test-" + (new Date().toISOString().replaceAll(":", "-")));
+fs.mkdirSync(testProjectResultsFolder, {recursive: true});
+const testLogFile = path.join(testProjectResultsFolder, "tests.log");
+logFile = fs.createWriteStream(testLogFile, {flags: "a"});
 
 cleanup();
 
-console.log("compiling test project");
+log("compiling test project");
 try {
-	child_process.execSync(`gcc -std=c17 -o ${COMPILATION_OUTPUT} -Wall -Wextra ${testProjectSource}`, {stdio: "inherit"});
-	console.log("project compiled successfully to ", COMPILATION_OUTPUT);
-	console.log("running tests");
+	child_process.execSync(`gcc -std=c17 -o ${COMPILATION_OUTPUT} -Wall -Wextra ${testProjectSource} ${LOG_TO_FILE ? `>> ${testLogFile} 2>&1` : ""}`, {stdio: "inherit"});
+	log("project compiled successfully to ", COMPILATION_OUTPUT);
+	log("running tests");
 
 	if (!fs.existsSync(COMPILATION_OUTPUT)) throw new Error("cannot find compiled project at " + COMPILATION_OUTPUT);
 	try {
-		child_process.execSync("npm run test", {stdio: "inherit"});
+		child_process.execSync(`npm run tui:test ${LOG_TO_FILE ? `>> ${testLogFile}` : ""}`, {stdio: "inherit"});
 	} catch (e) {
-		console.log("tests failed, running tui-test: ", e);
+		log("tests failed, running tui-test: ", e);
 	}
-	console.log("tests finished");
+	log("tests finished");
 
 	const testResults = {
 		project: testProjectFolder,
@@ -69,7 +82,7 @@ try {
 		percentage: 0,
 		points: 0
 	};
-	console.log("aggregating results");
+	log("aggregating results");
 	fs.readdirSync("results").forEach(file => {
 		if (!file.endsWith(".json")) return;
 		//console.log("processing result file: ", file);
@@ -80,21 +93,19 @@ try {
 	});
 	evaluateResults(testResults);
 
-	console.log("copying results to project");
-	const projectTestsFolder = path.join(testProjectFolder, "test-results", "test-" + (new Date().toISOString().replaceAll(":", "-")));
-	fs.mkdirSync(projectTestsFolder, {recursive: true});
-	fs.writeFileSync(path.join(projectTestsFolder, "test-results.json"), JSON.stringify(testResults, null, 2));
-	fs.mkdirSync(path.join(projectTestsFolder, "snapshots"));
+	log("copying results to project");
+	fs.writeFileSync(path.join(testProjectResultsFolder, "test-results.json"), JSON.stringify(testResults, null, 2));
+	fs.mkdirSync(path.join(testProjectResultsFolder, "snapshots"));
 	fs.readdirSync(path.join("tests", "__snapshots__")).forEach(snapshot => {
-		fs.copyFileSync(path.join("tests", "__snapshots__", snapshot), path.join(projectTestsFolder, "snapshots", snapshot));
+		fs.copyFileSync(path.join("tests", "__snapshots__", snapshot), path.join(testProjectResultsFolder, "snapshots", snapshot));
 	});
-	console.log("results copied successfully");
+	log("results copied successfully");
 
-	console.log("cleanup");
-	//cleanup();
-	console.log("done");
+	log("cleanup");
+	cleanup();
+	log("done");
 
 } catch (e) {
-	console.error(e);
+	log(e);
 	process.exit(1);
 }
